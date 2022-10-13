76fda935c4b9765d15a59f37aee0cf629d8a43c7
Karl Behrens <karlbehrensg@gmail.com>
2022-10-12 23:10:41 -0300

    Env example file

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/karlbehrensg/go-web-server-template/routes"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	router := gin.Default()
 	routes.ApplyRoutes(router)
-	router.Run()
+	router.Run(":" + port)
 }
